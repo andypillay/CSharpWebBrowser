91c22d7901bafe8985a9613fbf65af861ac906cf
Andy Pillay <andypillay93@gmail.com>
2016-03-09 21:22:51 +0000

    Added files via upload

---
+++ CSBrowser.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CSharpWebBrowser
+{
+    partial class CSBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolstripFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolstripExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolstripAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtAddressBar = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolstripFile,
+            this.toolstripAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(898, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolstripFile
+            // 
+            this.toolstripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolstripExit});
+            this.toolstripFile.Name = "toolstripFile";
+            this.toolstripFile.Size = new System.Drawing.Size(37, 20);
+            this.toolstripFile.Text = "File";
+            // 
+            // toolstripExit
+            // 
+            this.toolstripExit.Name = "toolstripExit";
+            this.toolstripExit.Size = new System.Drawing.Size(152, 22);
+            this.toolstripExit.Text = "Exit";
+            this.toolstripExit.Click += new System.EventHandler(this.toolstripExit_Click);
+            // 
+            // toolstripAbout
+            // 
+            this.toolstripAbout.Name = "toolstripAbout";
+            this.toolstripAbout.Size = new System.Drawing.Size(52, 20);
+            this.toolstripAbout.Text = "About";
+            this.toolstripAbout.Click += new System.EventHandler(this.toolstripAbout_Click);
+            // 
+            // txtAddressBar
+            // 
+            this.txtAddressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAddressBar.Location = new System.Drawing.Point(13, 24);
+            this.txtAddressBar.Name = "txtAddressBar";
+            this.txtAddressBar.Size = new System.Drawing.Size(820, 20);
+            this.txtAddressBar.TabIndex = 3;
+            this.txtAddressBar.Text = "http://";
+            this.txtAddressBar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAddressBar_KeyPress);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(838, 24);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(48, 21);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(13, 51);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(873, 444);
+            this.webBrowser.TabIndex = 5;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            this.webBrowser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 497);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(898, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 519);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtAddressBar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "C Sharp Web Browser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolstripFile;
+        private System.Windows.Forms.ToolStripMenuItem toolstripExit;
+        private System.Windows.Forms.ToolStripMenuItem toolstripAbout;
+        private System.Windows.Forms.TextBox txtAddressBar;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ CSBrowser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharpWebBrowser
+{
+    public partial class CSBrowser : Form
+    {
+        public CSBrowser()
+        {
+            InitializeComponent();
+        }
+
+
+        private void toolstripExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolstripAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Andy Pillay");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+           NavigateToPage();
+        }
+
+        private void txtAddressBar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                NavigateToPage();
+            }    
+        }
+
+        /// <summary>
+        /// Navigates to Page in addressbar.
+        /// Disables Go button and Address bar once Navigation is done.
+        /// </summary>
+        private void NavigateToPage()
+        {
+            btnGo.Enabled = false;
+            txtAddressBar.Enabled = false;
+            toolStripStatusLabel1.Text = "Navigating to " + txtAddressBar.Text;
+            
+        
+            webBrowser.Navigate(txtAddressBar.Text);
+        }
+
+        /// <summary>
+        /// Re-Enables Go and Address bar so new Page can be navigated once Document Completed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            btnGo.Enabled = true;
+            txtAddressBar.Enabled = true;
+            toolStripStatusLabel1.Text = "Navigation Complete";
+        }
+
+        private void webBrowser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            //Stops divide by zero error
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+            }           
+        }
+    }
+}
